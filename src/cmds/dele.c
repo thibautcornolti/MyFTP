1da97862d91ed01a6a178a8dfb9ab0f47147bd17
Thibaut Cornolti <thibaut@cornolti.eu>
2018-04-23 17:12:59 +0200

    remove

--- src/cmds/dele.c
+++ src/cmds/dele.c
@@ -9,8 +9,19 @@
 
 bool cmd_dele(sess_t *sess, char *line, net_t *client)
 {
-	(void) sess;
-	(void) line;
-	dprintf(client->fd, "331 User name okay, need password.\n");
+	char *path = get_arg(line, 1);
+	char *new;
+
+	if (!sess->logged) {
+		dprintf(client->fd, "530 Please login with USER and PASS.\n");
+		return (true);
+	}
+	transform_file_path(sess, &path, &new);
+	if (!remove(new))
+		dprintf(client->fd, "250 Requested file action okay, \
+completed.\n");
+	else
+		dprintf(client->fd, "550 Failed to remove file.\n");
+	free(new);
 	return (true);
 }
