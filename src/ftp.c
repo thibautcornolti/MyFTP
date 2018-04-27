be2af7d1aefaa144e13265d0bc2437a092610d4a
Thibaut Cornolti <thibaut@cornolti.eu>
2018-04-27 15:25:01 +0200

    unknown command

--- src/ftp.c
+++ src/ftp.c
@@ -22,17 +22,41 @@ static bool (*fcts[])(sess_t *, char *, net_t *) = {
 	&cmd_stor, &cmd_list, &cmd_type,
 };
 
+static bool is_full_space(char *line)
+{
+	size_t space_count = 0;
+
+	for (size_t i = 0 ; i < strlen(line) ; ++i)
+		if (line[i] != ' ' && line[i] != '\r' && line[i] != '\n')
+			return (false);
+		else if (line[i] == ' ')
+			space_count++;
+	return (space_count > 0);
+}
+
+static void print_unknown_command(sess_t *sess, net_t *client)
+{
+	if (sess->logged)
+		dprintf(client->fd, "500 Unknown command.\n");
+	else
+		dprintf(client->fd, "530 Please login with USER and PASS.\n");
+}
+
 bool commander(sess_t *sess, char *line, net_t *client)
 {
 	char *cmd = to_upper_case(get_arg(line, 0));
 	int ret = -1;
 
+	if (is_full_space(line)) {
+		print_unknown_command(sess, client);
+		return (true);
+	}
 	if (!strlen(cmd))
 		return (true);
 	for (size_t i = 0 ; refs[i] ; ++i)
 		if (!strcmp(refs[i], cmd))
 			ret = fcts[i](sess, line, client);
 	if (ret == -1)
-		dprintf(client->fd, "500 Unknown command.\n");
+		print_unknown_command(sess, client);
 	return (ret != 0);
 }
